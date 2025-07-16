e1a6fe9cadc1cab403ceb236a046c540ad17cde4
Joel Kingsley <joelkingsley.r@gmail.com>
2025-07-16 20:32:43 +0200

    Added assets

--- SDOMultiplatform/iosApp/iosApp/ContentView.swift
+++ SDOMultiplatform/iosApp/iosApp/ContentView.swift
@@ -1,33 +1,69 @@
 import SwiftUI
 import Shared
 
-struct ContentView: View {
-    @State private var showContent = false
-    var body: some View {
-        VStack {
-            Button("Click me!") {
-                withAnimation {
-                    showContent = !showContent
-                }
-            }
+//struct ContentView: View {
+//    @State private var showContent = false
+//    var body: some View {
+//        VStack {
+//            Button("Click me!") {
+//                withAnimation {
+//                    showContent = !showContent
+//                }
+//            }
+//
+//            if showContent {
+//                VStack(spacing: 16) {
+//                    Image(systemName: "swift")
+//                        .font(.system(size: 200))
+//                        .foregroundColor(.accentColor)
+//                    Text("SwiftUI: \(Greeting().greet())")
+//                }
+//                .transition(.move(edge: .top).combined(with: .opacity))
+//            }
+//        }
+//        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+//        .padding()
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
 
-            if showContent {
-                VStack(spacing: 16) {
-                    Image(systemName: "swift")
-                        .font(.system(size: 200))
-                        .foregroundColor(.accentColor)
-                    Text("SwiftUI: \(Greeting().greet())")
-                }
-                .transition(.move(edge: .top).combined(with: .opacity))
+struct ContentView: View {
+    @State var isNavigationBarHidden: Bool = true
+    @EnvironmentObject var authViewModel: AuthenticationViewModel
+    
+    var body: some View {
+        return Group {
+            switch authViewModel.state {
+            case .signedIn:
+              #if os(iOS)
+                AppTabNavigation(isNavigationBarHidden: self.$isNavigationBarHidden)
+              #else
+                NotAvailableView()
+              #endif
+            case .signedOut:
+              #if os(iOS)
+                SignInView()
+              #else
+                NotAvailableView()
+              #endif
+            case .loading:
+              #if os(iOS)
+                ProgressView("progressViewLoadingLabel")
+                    .progressViewStyle(.circular)
+              #else
+                NotAvailableView()
+              #endif
             }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        .navigationBarTitle("")
+        .navigationBarHidden(self.isNavigationBarHidden)
+        .onAppear {
+            self.isNavigationBarHidden = true
+        }
     }
 }
--- SDOMultiplatform/iosApp/iosApp/iOSApp.swift
+++ SDOMultiplatform/iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,35 @@
 import SwiftUI
+import GoogleSignIn
+
+//@main
+//struct iOSApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
+
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @StateObject var authViewModel = AuthenticationViewModel()
+    
+    init() {
+        SDOTheme.initializeTheme()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+          ContentView()
+            .environmentObject(authViewModel)
+            .onAppear {
+                authViewModel.restorePreviousSignIn()
+            }
+            .onOpenURL { url in
+              GIDSignIn.sharedInstance.handle(url)
+            }
         }
     }
 }
